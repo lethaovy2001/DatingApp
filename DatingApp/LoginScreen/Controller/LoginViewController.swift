f156f24659752e5cf4f2282227e32b47de927dd1
Vy Le <lethaovy2001@gmail.com>
2020-04-12 13:30:50 -0500

    update user info to cloud firestore

--- DatingApp/LoginScreen/Controller/LoginViewController.swift
+++ DatingApp/LoginScreen/Controller/LoginViewController.swift
@@ -18,10 +18,12 @@ class LoginViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    private var db: Firestore!
     
     // MARK: Life Cycles
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
+        db = Firestore.firestore()
         setupUI()
         mainView.setLoginSelector(selector: #selector(loginWithFacebook), target: self)
     }
@@ -42,32 +44,18 @@ class LoginViewController: UIViewController {
         view.backgroundColor = .white
     }
     
-    private func calculateAge(birthday: String) {
+    private func calculateAge(birthday: String) -> Int {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MM-dd-yyyy"
-        if let date = dateFormatter.date(from: birthday) {
-            let now = Date()
-            let calendar = Calendar.current
-            let ageComponents = calendar.dateComponents([.year], from: date, to: now)
-            let age = ageComponents.year!
-        }
-    }
-
-    private func getFBUserInfo() {
-        GraphRequest(graphPath: "/me", parameters: ["fields": "first_name, email, birthday, gender"]).start { (connection, result, err) in
-            if err != nil {
-                print("Failed to start graph request:", err)
-                return
-            }
-            let fbDetails = result as! NSDictionary
-            if let birthday = fbDetails["birthday"] as? String {
-                self.calculateAge(birthday: birthday)
-            }
-            print(fbDetails)
-            self.navigationController?.popToRootViewController(animated: false)
-        }
+        guard let date = dateFormatter.date(from: birthday) else { return 0 }
+        let now = Date()
+        let calendar = Calendar.current
+        let ageComponents = calendar.dateComponents([.year], from: date, to: now)
+        let age = ageComponents.year!
+        return age
     }
     
+    // MARK: Firebase
     private func authenticateWithFirebase(_ completion : @escaping()->()) {
         let credential = FacebookAuthProvider.credential(withAccessToken: AccessToken.current!.tokenString)
         Auth.auth().signIn(with: credential) { (authResult, error) in
@@ -77,6 +65,37 @@ class LoginViewController: UIViewController {
             }
             print("Successfully log user into firebase")
             completion()
+        }
+    }
+    
+    private func updateDatabase(with uid: String, values: [String: AnyObject]) {
+        db.collection("users").document(uid).setData(values) { err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+            }
+        }
+    }
+    
+    // MARK: Facebook
+    private func getFBUserInfo() {
+        GraphRequest(graphPath: "/me", parameters: ["fields": "first_name, email, birthday, gender"]).start { (connection, result, err) in
+            if err != nil {
+                print("Failed to start graph request:", err!)
+                return
+            }
+            let fbDetails = result as! NSDictionary
+            guard let userID = Auth.auth().currentUser?.uid else { return }
+            guard let firstName = fbDetails["first_name"] else { return }
+            guard let gender = fbDetails["gender"] else { return }
+            guard let birthday = fbDetails["birthday"] as? String else { return }
+            let age = self.calculateAge(birthday: birthday)
+            let dictionary = ["uid": userID,
+            "first_name": firstName,
+            "age": age,
+            "gender": gender] as [String : AnyObject]
+            self.updateDatabase(with: userID, values: dictionary)
         }
     }
     
@@ -93,9 +112,14 @@ class LoginViewController: UIViewController {
                 UserDefaults.standard.synchronize()
                 self.authenticateWithFirebase {
                     self.getFBUserInfo()
+                    self.goToMainViewController()
                 }
             }
         }
+    }
+    
+    private func goToMainViewController() {
+        self.navigationController?.popToRootViewController(animated: false)
     }
 }
 
