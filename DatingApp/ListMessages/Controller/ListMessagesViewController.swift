f87165c1a0b69b22c76d18a5f9185e5ed1af1efa
Vy Le <lethaovy2001@gmail.com>
2020-05-17 17:06:40 -0500

    resolve merge conflicts

--- DatingApp/ListMessages/Controller/ListMessagesViewController.swift
+++ DatingApp/ListMessages/Controller/ListMessagesViewController.swift
@@ -91,7 +91,7 @@ extension ListMessagesViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = ChatViewController()
-        //vc.user = modelController.getUsers()[indexPath.item]
+        vc.user = modelController.getUsers()[indexPath.item]
         self.navigationController?.pushViewController(vc, animated: true)
     }
 }
