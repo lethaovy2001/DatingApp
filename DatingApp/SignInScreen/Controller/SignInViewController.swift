eb90766fd08c372631d39e8d192889644ddad5b4
Vy Le <lethaovy2001@gmail.com>
2020-05-17 00:10:51 -0500

    review before merge

--- DatingApp/SignInScreen/Controller/SignInViewController.swift
+++ DatingApp/SignInScreen/Controller/SignInViewController.swift
@@ -52,7 +52,6 @@ class SignInViewController: UIViewController {
     //MARK: Actions
     @objc func signIn() {
         guard let email = mainView.getEmailText(), let password = mainView.getPasswordText(), let name = mainView.getNameText() else {
-            self.mainView.showError(message: "Missing some fields")
             return
         }
         firebaseService.createUser(email: email, password: password, { errorMessage in
