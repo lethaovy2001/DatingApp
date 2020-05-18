223bb1e3def354926e587d4c6e0e5881f2638b4b
Vy Le <lethaovy2001@gmail.com>
2020-05-18 01:25:08 -0500

    test UserModel and UserViewModel

--- DatingAppTests/UserViewModelTest.swift
+++ DatingAppTests/UserViewModelTest.swift
@@ -11,15 +11,62 @@ import XCTest
 
 class UserViewModelTest: XCTestCase {
     
-    func testViewModel() {
-        let model = UserModel(name: "Vy", birthday: Date(), work: "UW Hospital", bio: "Sweet but psycho", gender: "female", images: [UIImage(named: "Vy.jpg")!])
-        let userViewModel = UserDetailsViewModel(model: model)
-        
-        XCTAssertEqual(userViewModel.name, "Vy")
-        XCTAssertEqual(userViewModel.ageText, "0")
-        XCTAssertEqual(userViewModel.work, "UW Hospital")
-        XCTAssertEqual(userViewModel.bio, "Sweet but psycho")
-        XCTAssertEqual(userViewModel.images, [UIImage(named: "Vy.jpg")!])
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testViewModel() {
+        let info: [String: Any] = [
+            "id": "Id",
+            "first_name": "First Name",
+            "birthday": Date(),
+            "work": "Work",
+            "bio": "Bio",
+        ]
+        let image = UIImage(named: "Vy")!
+        let images = [image, image]
+        let model = UserModel(info: info, images: images)
+        let viewModel = UserDetailsViewModel(model: model)
+        XCTAssertEqual(viewModel.id, "Id")
+        XCTAssertEqual(viewModel.name, "First Name")
+        XCTAssertEqual(viewModel.ageText, "0")
+        XCTAssertEqual(viewModel.bio, "Bio")
+        XCTAssertEqual(viewModel.work, "Work")
+        XCTAssertEqual(viewModel.images, images)
+    }
+    
+    func testNilViewModel() {
+        let info: [String: Any?] = [
+            "id": nil,
+            "first_name": nil,
+            "birthday": nil,
+            "work": nil,
+            "bio": nil,
+        ]
+        let images: [UIImage] = []
+        let model = UserModel(info: info, images: images)
+        let viewModel = UserDetailsViewModel(model: model)
+        XCTAssertEqual(viewModel.id, nil)
+        XCTAssertEqual(viewModel.name, "")
+        XCTAssertEqual(viewModel.ageText, "")
+        XCTAssertEqual(viewModel.bio, "")
+        XCTAssertEqual(viewModel.work, "")
+        XCTAssertEqual(viewModel.images, images)
+    }
 }
