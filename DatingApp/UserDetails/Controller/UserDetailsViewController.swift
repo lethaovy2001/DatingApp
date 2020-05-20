5fd982339b2113280ba7adf0c1c9ac460deb0688
Vy Le <lethaovy2001@gmail.com>
2020-05-20 18:00:05 -0500

    add comments in UserDetailsViewController

--- DatingApp/UserDetails/Controller/UserDetailsViewController.swift
+++ DatingApp/UserDetails/Controller/UserDetailsViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class UserDetailsViewController: UIViewController {
+    // MARK: Properties
     private let userDetailsView = UserDetailsView()
     private let modelController = UserDetailsModelController()
-    private let firebaseService = FirebaseService()
     var viewModel: UserDetailsViewModel?
     
-    // MARK: Life Cycles
+    // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -26,9 +26,8 @@ class UserDetailsViewController: UIViewController {
         reloadUserInfo()
     }
     
-    // MARK: Setup
+    // MARK: - Setup
     private func setup() {
-        addNavigationBar()
         setupUI()
         setSelectors()
     }
@@ -49,22 +48,15 @@ class UserDetailsViewController: UIViewController {
         userDetailsView.setBackButtonSelector(selector: #selector(backButtonPressed), target: self)
     }
     
-    func addNavigationBar() {
-        let navBar = self.navigationController?.navigationBar
-        let navItem = self.navigationItem
-        navBar?.tintColor = UIColor.amour
-        navItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editButtonPressed))
-    }
-    
     // MARK: Actions
-    @objc func editButtonPressed() {
+    @objc private func editButtonPressed() {
         if let viewModel = viewModel {
             let vc = EditUserDetailsViewController(viewModel: viewModel)
             self.navigationController?.pushViewController(vc, animated: false)
         }
     }
     
-    @objc func backButtonPressed() {
+    @objc private func backButtonPressed() {
         self.navigationController?.popToRootViewController(animated: true)
     }
     
